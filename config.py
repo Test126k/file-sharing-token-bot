ed5328de46f811754bd3c46a80696f6c18c782c0
Test126k <test126126k@gmail.com>
2025-02-19 17:51:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -45,7 +45,7 @@ TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nI can store private files in Specified Channel and other users can access it from special link.")
 try:
     ADMINS=[]
-    for x in (os.environ.get("ADMINS", "6057768840 ").split()):
+    for x in (os.environ.get("ADMINS", "6057768840 5295407868").split()):
         ADMINS.append(int(x))
 except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
@@ -66,7 +66,7 @@ BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
 USER_REPLY_TEXT = "❌Don't send me messages directly I'm only File Share bot!"
 
 ADMINS.append(OWNER_ID)
-ADMINS.append(6695586027)
+ADMINS.append(6057768840)
 
 LOG_FILE_NAME = "filesharingbot.txt"
 
